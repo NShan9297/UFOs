6bd086be527869d92e80f432cdfa872a73a4e913
NShan9297 <99927324+NShan9297@users.noreply.github.com>
2022-06-03 22:49:14 -0400

    updating code

---
+++ static/js/practice.js
@@ -0,0 +1,16 @@
+let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+for  (var i= 0; i< vegetables.length; i++){
+  console.log("I love " +  vegetables[i] );
+}
+
+// i in this instance is referencing the iteration
+
+for (var i = 0; i < 5; i++) {
+  console.log("I am " + i);
+}
+
+let numbers = [1,2,3,4,5,6];
+numbers.forEach(function(element){
+  console.log(element**2);
+
+})
